6aa1af27c38b407983c7ab3a69947e6ba36f27bb
Ich <Ich@Ich.fritz.box>
2019-04-15 23:46:35 +0200

    kek

---
+++ MySQL_Test/src/TestDatabase.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestDatabase {
+
+	public static void main(String[] args) throws SQLException {
+		// db parameters
+		String url       = "jdbc:mysql://localhost:3306/atg_winterlauf_2019?serverTimezone=Europe/Berlin";
+		String user      = "root";
+		String password  = "";
+		
+		Connection conn = DriverManager.getConnection(url, user, password);
+		
+		Statement stm = conn.createStatement();
+	 	ResultSet rs = stm.executeQuery("SELECT * FROM tbl_standort");
+	 	while (rs.next()) {
+			System.out.println(rs.getString("Ort"));
+		}
+
+	}
+
+}

36c30a44c94956c05c95f57b8fc9269e68850fb3
Ich <Ich@Ich.fritz.box>
2019-04-16 12:36:29 +0200

    kek

--- MySQL_Test/src/DatabaseHandler.java
+++ MySQL_Test/src/DatabaseHandler.java
@@ -4,7 +4,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class TestDatabase {
+public class DatabaseHandler {
 
 	public static void main(String[] args) throws SQLException {
 		// db parameters
---
+++ MySQL_Test/src/ServerSock.java
@@ -0,0 +1,9 @@
+
+public class ServerSock {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
